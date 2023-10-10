7b84e49446a7428825081cf5e313f36bf146c0e5
omarikas <86628361+omarikas@users.noreply.github.com>
2023-10-10 23:41:37 +0300

    Update doctorvariables.js

--- model/doctorvariables.js
+++ model/doctorvariables.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:model/doctorvariables.js
 const mongoose = require('mongoose')
 const Patients = require('./patientvariables')
 const Schema = mongoose.Schema
@@ -65,3 +66,71 @@ const doctorSchema = new Schema({
 
 const doctor= mongoose.model('request', doctorSchema);
 module.exports = doctor;
+=======
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const doctorSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    }
+    ,
+    name: {
+        type: String,
+        required: true
+    }
+    ,
+    email: {
+        type: String,
+        required: true
+    }
+
+    ,
+    password: {
+        type: String,
+        required: true
+    }
+    ,
+    date: {
+        type: String,
+        required: true
+    }
+
+    ,
+    education: {
+        type: String,
+        required: true
+    }
+    ,
+    rate: {
+        type: Number,
+        required: true
+    }
+
+    ,
+    hospital: {
+        type: String,
+        required: true
+    }
+
+    ,
+    patients: {
+        type: Array,
+        required: true
+    }
+
+    ,
+
+    doctorid: {
+        type: String,
+        required: true
+    }
+
+
+
+
+
+
+
+})
+>>>>>>> Stashed changes:doctorvariables.js
